6302c347e2280d0664f529644f5132aeed79b604
SuhelPathan17 <119145802+SuhelPathan17@users.noreply.github.com>
2023-08-07 10:40:59 +0530

    Add files via upload

---
+++ Problems_on_Pattern_Printing/Program93.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    public:
+        int iValue;
+
+        Pattern(int i)
+        {
+            iValue = i;
+        }
+
+        void DisplayPattern()
+        {
+            if(iValue < 0)
+            {
+                iValue = -iValue;
+            }
+            
+            int iCnt = 0;
+            for(iCnt = 1; iCnt <= iValue; iCnt++)
+            {
+                cout<<iCnt<<"\t"<<"*\t";
+            }
+            cout<<"\n";
+        }
+};
+
+int main()
+{
+    int iNo = 0;
+    Pattern *ptr = NULL;
+
+    cout<<"Enter the number : \n";
+    cin>>iNo;
+
+    ptr = new Pattern(iNo);
+    ptr->DisplayPattern();
+    
+    return 0;
+}
---
+++ Problems_on_Pattern_Printing/Program96.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    public:
+        int iValue;
+
+        Pattern(int i)
+        {
+            iValue = i;
+        }
+
+        void DisplayPattern()
+        {
+            if(iValue < 0)
+            {
+                iValue = -iValue;
+            }
+            
+            int iCnt = 0;
+            char ch = 'A';
+
+            for(iCnt = 1; iCnt <= iValue; iCnt++)
+            {
+                cout<<ch<<"\t";
+                ch++;
+            }
+
+        }
+};
+
+int main()
+{
+    int iNo = 0;
+    Pattern *ptr = NULL;
+
+    cout<<"Enter the number : \n";
+    cin>>iNo;
+
+    ptr = new Pattern(iNo);
+    ptr->DisplayPattern();
+    
+    return 0;
+}
