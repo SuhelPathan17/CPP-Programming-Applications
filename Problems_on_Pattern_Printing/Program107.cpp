1c74c1fb54a7385a8c45b4dd5d197732b6f072d0
SuhelPathan17 <119145802+SuhelPathan17@users.noreply.github.com>
2023-08-10 17:15:14 +0530

    Add files via upload

---
+++ Problems_on_Pattern_Printing/Program107.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    public:
+        int iRow;
+        int iCol;
+
+        Pattern(int r, int c)
+        {
+            iRow = r;
+            iCol = c;
+        }
+
+        void DisplayPattern()
+        {
+            int i = 0;
+            int j = 0;
+
+            if(iRow < 0)
+            {
+                iRow = -iRow;
+            }
+
+            if(iCol < 0)
+            {
+                iCol = -iCol;
+            }
+            
+            for(i = 1; i<=iRow; i++)
+            {
+                for(j = 1; j<=iCol; j++)
+                {
+                    if(j % 2 == 0)
+                    {
+                        cout<<"#\t";
+                    }
+                    else
+                    {
+                        cout<<j<<"\t";
+                    }
+                }
+                cout<<"\n";
+            }
+
+        }
+};
+
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+
+    Pattern *ptr = NULL;
+
+    cout<<"Enter the row size : \n";
+    cin>>iNo1;
+
+    cout<<"Enter the column size : \n";
+    cin>>iNo2;
+
+    ptr = new Pattern(iNo1,iNo2);
+    ptr->DisplayPattern();
+    
+    return 0;
+}
